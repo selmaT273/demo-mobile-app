1cebd026953d648afa694936d5bce28b7eb6f65b
Stacey Teltser <stacey.teltser@gmail.com>
2021-12-17 11:35:05 -0600

    bring in react-native-vector-icons and use for home tab

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import HomeScreen from './HomeScreen';
 import CalendarScreen from './CalendarScreen';
 
@@ -16,7 +17,16 @@ export default function App() {
           tabBarActiveTintColor: 'pink',
         }}
       >
-        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({color, size}) => (
+              <MaterialCommunityIcons name="home" color={color} size={size} />
+            ),
+            }}
+          />
         <Tab.Screen name="Calendar" component={CalendarScreen} />
       </Tab.Navigator>
     </NavigationContainer>
